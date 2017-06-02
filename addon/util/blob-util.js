aae2104b5cf79bb1fe0a68501dc043a205df4574
ampatspell <ampatspell@gmail.com>
2017-06-02 18:52:15 +0300

    import blob-util w/o ember-browserify

--- addon/util/blob-util.js
+++ addon/util/blob-util.js
@@ -1,1 +1,2 @@
-export default {};
+const blobUtil = window.blobUtil;
+export default blobUtil;
--- index.js
+++ index.js
@@ -5,5 +5,44 @@ module.exports = {
   name: 'couch',
   isDevelopingAddon() {
     return true;
-  }
+  },
+  treeForVendor: function() {
+    return treeForBlobUtil();
+  },
+  included: function(app) {
+    app.import('vendor/blob-util.js');
+  },
 };
+
+var path = require('path');
+var Template = require('broccoli-templater');
+var stew = require('broccoli-stew');
+var rename = stew.rename;
+var find = stew.find;
+
+function treeForBlobUtil() {
+  var blobUtilPath = cleanup(require.resolve('blob-util'));
+  var expandedBlobUtilPath = expand(blobUtilPath);
+  var blobUtil = normalizeFileName(find(expandedBlobUtilPath));
+  return new Template(blobUtil, blobUtilPath, function(content) {
+    return {
+      moduleBody: content
+    };
+  });
+}
+
+function cleanup(input) {
+  return input.replace('lib/index.js', 'dist/blob-util.js');
+}
+
+function normalizeFileName(tree) {
+  return rename(tree, function() {
+    return 'blob-util.js';
+  });
+}
+
+function expand(input) {
+  var dirname = path.dirname(input);
+  var file = path.basename(input);
+  return dirname + '/{' + file + '}';
+}
