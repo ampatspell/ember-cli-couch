0ccdd776479dd745c00b179cf84dad42f41e1fb0
ampatspell <ampatspell@gmail.com>
2017-06-02 20:07:56 +0300

    add 1.6 and 2.0 proxies

---
+++ server/proxies/api.js
@@ -0,0 +1,25 @@
+module.exports = app => {
+
+  let request = require('request');
+
+  let add = (path, base) => {
+    app.use(path, (req, res, next) => {
+      var target = `${base}${req.url}`;
+      req.pipe(request(target).on('error', err => {
+        var error = new Error("Proxy");
+        error.method = req.method;
+        error.url = target;
+        error.reason = err.message;
+        next(error);
+      })).pipe(res);
+    });
+
+    let stack = app._router.stack;
+    let m = stack.pop();
+    stack.unshift(m);
+  };
+
+  add('/api/1.6', 'http://127.0.0.1:3984');
+  add('/api/2.0', 'http://127.0.0.1:5984');
+
+};
