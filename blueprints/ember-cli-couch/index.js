8c8d07f857e171cced765d017ebfd10265793dc7
ampatspell <ampatspell@gmail.com>
2017-06-03 19:59:42 +0300

    move blob-util to dependencies and don't add to app

--- blueprints/ember-cli-couch/index.js
+++ blueprints/ember-cli-couch/index.js
@@ -1,12 +1,7 @@
-var RSVP = require('rsvp');
-
 module.exports = {
   normalizeEntityName: function() {
   },
   afterInstall: function() {
-    return RSVP.all([
-      this.addPackageToProject('blob-util', '1.2.1'),
-      this.addAddonToProject('ember-network', '^0.3.1')
-    ]);
+    return this.addAddonToProject('ember-network', '^0.3.1')
   }
 };
