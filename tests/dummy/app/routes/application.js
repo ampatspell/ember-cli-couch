61c214a28647bef76c659e353ea0da2c9583cb53
ampatspell <ampatspell@gmail.com>
2017-06-11 19:01:10 +0300

    dummy

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
+const {
+  Logger: { error }
+} = Ember;
+
 export default Ember.Route.extend({
   model() {
-    return this.get('db.database').create({ optional: true });
+    return this.get('db.database').create({ optional: true }).catch(err => {
+      error('routes/application.js', err.toJSON ? err.toJSON() : err);
+    });
   }
 });
