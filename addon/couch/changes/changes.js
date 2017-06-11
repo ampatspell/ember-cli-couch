52e62cf175fbe029a28aad97678272fc5e360cbc
ampatspell <ampatspell@gmail.com>
2017-06-11 19:00:53 +0300

    changes base class as a factory for additional recreate filter deps

--- addon/couch/changes/changes.js
+++ addon/couch/changes/changes.js
@@ -9,79 +9,73 @@ const {
   Evented
 } = Ember;
 
-export default Ember.Object.extend(Evented, {
+export default (...deps) => {
+  return Ember.Object.extend(Evented, {
 
-  enabled: false,
-  feed: 'event-source',
+    enabled: false,
+    feed: 'event-source',
 
-  _feedOptions() {
-    assert({ error: 'internal', reason: 'override Changes._feedOptions' }, false);
-  },
+    _feedOptions() {
+      assert({ error: 'internal', reason: 'override Changes._feedOptions' }, false);
+    },
 
-  _feedFactoryName() {
-    assert({ error: 'internal', reason: 'override Changes._feedFactoryName' }, false);
-  },
+    _feedFactoryName() {
+      assert({ error: 'internal', reason: 'override Changes._feedFactoryName' }, false);
+    },
 
-  _lookupFeedClass(feed) {
-    let name = this._feedFactoryName(feed);
-    let factory = getOwner(this).factoryFor(name);
-    assert(`changes feed factory named '${name}' is not registered`, !!factory);
-    return factory.class;
-  },
+    _lookupFeedClass(feed) {
+      let name = this._feedFactoryName(feed);
+      let factory = getOwner(this).factoryFor(name);
+      assert(`changes feed factory named '${name}' is not registered`, !!factory);
+      return factory.class;
+    },
 
-  _createFeed(feed) {
-    let Class = this._lookupFeedClass(feed);
-    let opts = this._feedOptions();
-    let instance = new Class(opts);
-    instance._feed = feed;
-    return instance;
-  },
+    _createFeed(feed) {
+      let Class = this._lookupFeedClass(feed);
+      let opts = this._feedOptions();
+      let instance = new Class(opts);
+      return instance;
+    },
 
-  _updateFeed() {
-    let { feed, enabled, _feed } = this.getProperties('feed', 'enabled', '_feed');
-    if(feed && enabled) {
+    _updateFeed() {
+      let { feed, enabled, _feed } = this.getProperties('feed', 'enabled', '_feed');
       if(_feed) {
-        if(_feed._feed === feed) {
-          return;
-        }
+        _feed.destroy();
+        _feed = null;
+      }
+      if(feed && enabled) {
+        _feed = this._createFeed(feed);
+        _feed.delegate = this;
+        _feed.start();
+      }
+      this.set('_feed', _feed);
+    },
+
+    _setSource: on('init', function() {
+      this._updateFeed();
+    }),
+
+    _sourceDependenciesDidChange: observer('feed', 'enabled', ...deps, function() {
+      console.log('deps did change');
+      cancel(this.__updateFeed);
+      this.__updateFeed = next(() => this._updateFeed());
+    }),
+
+    willDestroy() {
+      cancel(this.__updateFeed);
+      let _feed = this.get('_feed');
+      if(_feed) {
         _feed.destroy();
       }
-      _feed = this._createFeed(feed);
-      _feed.delegate = this;
-      this.set('_feed', _feed);
-      _feed.start();
-    } else {
-      if(!_feed) {
-        return;
-      }
-      _feed.destroy();
-      this.set('_feed', null);
+    },
+
+    onData(feed, data) {
+      this.trigger('data', data);
+    },
+
+    onError(feed, err) {
+      this.trigger('error', err);
     }
-  },
 
-  _setSource: on('init', function() {
-    this._updateFeed();
-  }),
-
-  _sourceDependenciesDidChange: observer('feed', 'enabled', function() {
-    cancel(this.__updateFeed);
-    this.__updateFeed = next(() => this._updateFeed());
-  }),
-
-  willDestroy() {
-    cancel(this.__updateFeed);
-    let _feed = this.get('_feed');
-    if(_feed) {
-      _feed.destroy();
-    }
-  },
-
-  onData(feed, data) {
-    this.trigger('data', data);
-  },
-
-  onError(feed, err) {
-    this.trigger('error', err);
-  }
-
-});
+  });
+}
--- addon/couch/database/changes.js
+++ addon/couch/database/changes.js
@@ -1,15 +1,29 @@
-import Changes from '../changes/changes';
+import create from '../changes/changes';
+
+const Changes = create('view');
 
 export default Changes.extend({
 
   database: null,
 
+  view: null,
+
   _feedOptions() {
+    let view = this.get('view');
+    if(!view) {
+      view = undefined;
+    }
+    let filter;
+    if(view) {
+      filter = '_view';
+    }
     return {
       url: `${this.get('database.url')}/_changes`,
       qs: {
-        include_docs: true
-      }
+        include_docs: true,
+        view,
+        filter
+      },
     };
   },
 
