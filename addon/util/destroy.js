7791259d488547fdbb8212e29b86a405a1610402
ampatspell <ampatspell@gmail.com>
2017-06-13 00:13:48 +0300

    destroyArray util

--- addon/util/destroy.js
+++ addon/util/destroy.js
@@ -1,6 +1,19 @@
+import Ember from 'ember';
+
+const {
+  A
+} = Ember;
+
 export function destroyObject(object) {
   for(let key in object) {
     object[key].destroy();
     delete object[key];
   }
+}
+
+export function destroyArray(array) {
+  A(array).map(item => {
+    item.destroy();
+  });
+  array.clear();
 }
